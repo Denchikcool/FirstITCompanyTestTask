c3a1c49e288c5f1bfbec4dbe591007bfa66c195b
Denchikcool <kuzmenok.den@yandex.ru>
2026-03-10 16:24:00 +0700

    Созданы пути для каждой страницы CRUD

---
+++ finance/urls.py
@@ -0,0 +1,9 @@
+from django.urls import path
+from . import views
+
+urlpatterns = [
+    path('', views.transaction_list, name='transactions'),
+    path('create/', views.transaction_create, name='create'),
+    path('edit/<int:pk>/', views.transiction_update, name='edit'),
+    path('delete/<int:pk>/', views.transaction_delete, name='delete')
+]
