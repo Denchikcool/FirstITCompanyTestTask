736451cc74012c5dda98825ca9c87c35a282a4d5
Denchikcool <kuzmenok.den@yandex.ru>
2026-03-10 17:07:38 +0700

    Название метода написал правильно

--- finance/urls.py
+++ finance/urls.py
@@ -4,6 +4,6 @@ from . import views
 urlpatterns = [
     path('', views.transaction_list, name='transactions'),
     path('create/', views.transaction_create, name='create'),
-    path('edit/<int:pk>/', views.transiction_update, name='edit'),
+    path('edit/<int:pk>/', views.transaction_update, name='edit'),
     path('delete/<int:pk>/', views.transaction_delete, name='delete')
 ]
