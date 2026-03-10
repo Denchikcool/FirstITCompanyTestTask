91031f8405c8bd40d5a760f7d8e22ecfe9833555
Denchikcool <kuzmenok.den@yandex.ru>
2026-03-10 16:12:01 +0700

    Реализована логика CRUD

--- finance/views.py
+++ finance/views.py
@@ -1,3 +1,62 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect, get_object_or_404
+from .models import Transaction
+from .forms import TransactionForm
+from .filters import TransactionFilter
 
-# Create your views here.
+def transaction_list(request):
+    qs = Transaction.objects.all().order_by('-created_at')
+
+    transaction_filter = TransactionFilter(request.GET, queryset=qs)
+
+    return render(
+        request,
+        'transactions_list.html',
+        {
+            'filter': transaction_filter
+        }
+    )
+
+def transaction_create(request):
+    if request.method == "POST":
+        form = TransactionForm(request.POST)
+
+        if form.is_valid():
+            form.save()
+            return redirect('transactions')
+    else:
+        form = TransactionForm()
+
+    return render(
+        request,
+        'transiction_form.html',
+        {
+            'form': form
+        }
+    )
+
+def transiction_update(request, pk):
+    transaction = get_object_or_404(Transaction, pk=pk)
+
+    if request.method == "POST":
+        form = TransactionForm(request.POST, pk=pk)
+
+        if form.is_valid():
+            form.save()
+            return redirect('transactions')
+    else:
+        form = TransactionForm(instance=transaction)
+
+    return render(
+        request,
+        'transaction_form.html',
+        {
+            'form': form
+        }
+    )
+
+def transaction_delete(request, pk):
+    transaction = get_object_or_404(Transaction, pk=pk)
+
+    transaction.delete()
+
+    return redirect('transactions')
